daa1190e309315eaffb1b8c83f5f443d70b1e0ee
sovnya <enp7s1@tutanota.com>
2023-12-04 18:28:56 +0100

    feat: Add basic SixthSense component
    No fucking clue how to make the updating work, lol

--- SixthSense/Plugin.cs
+++ SixthSense/Plugin.cs
@@ -1,20 +1,38 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 using SixthSense.Buttplug;
+using UnityEngine;
 
 namespace SixthSense
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class SixthSense : BaseUnityPlugin
     {
-        internal static DeviceManager DeviceManager { get; private set; }
+        public static DeviceManager DeviceManager { get; private set; }
+        public static ManualLogSource Log { get; private set; }
 
         private void Awake()
         {
+            Log = Logger;
+
             DeviceManager = new DeviceManager(Logger, "SixthSense");
             DeviceManager.ConnectDevices();
 
-            // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} has loaded!");
+        }
+    }
+
+    public class SixthSenseComponent : MonoBehaviour
+    {
+        void Awake()
+        {
+            SixthSense.Log.LogInfo("Awoke.");
+        }
+
+        void Update()
+        {
+            SixthSense.Log.LogInfo("called");
         }
     }
 }
